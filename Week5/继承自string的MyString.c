974590c1d7c64156325c6eec82f1d450c3cfb82f
Reanon <793584285@qq.com>
2017-12-01 15:46:21 +0800

    Create 继承自string的MyString.c

---
+++ Week5/继承自string的MyString.c
@@ -0,0 +1,78 @@
+// week5_021.cpp: 定义控制台应用程序的入口点。
+//
+/*
+输入
+无
+
+输出
+1. abcd-efgh-abcd-
+2. abcd-
+3.
+4. abcd-efgh-
+5. efgh-
+6. c
+7. abcd-
+8. ijAl-
+9. ijAl-mnop
+10. qrst-abcd-
+11. abcd-qrst-abcd- uvw xyz
+about
+big
+me
+take
+abcd
+qrst-abcd-
+*/
+#include "stdafx.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+class MyString :public string
+{
+	// 在此处补充你的代码
+public:
+	MyString() :string() { }
+	MyString(const char *s) :string(s) {  }
+	MyString(const string &s):string (s){}//复制构造函数这里很神奇，比较迷。。。。。。
+	MyString operator()(int s, int l) {
+		return substr(s, l);
+	}	
+	// 在此处补充你的代码
+};
+
+
+int main()
+{
+	MyString s1("abcd-"), s2, s3("efgh-"), s4(s1);
+	MyString SArray[4] = { "big","me","about","take" };
+	cout << "1. " << s1 << s2 << s3 << s4 << endl;
+	s4 = s3;
+	s3 = s1 + s3;   
+	cout << "2. " << s1 << endl;
+	cout << "3. " << s2 << endl;
+	cout << "4. " << s3 << endl;
+	cout << "5. " << s4 << endl;
+	cout << "6. " << s1[2] << endl;
+	s2 = s1;
+	s1 = "ijkl-";
+	s1[2] = 'A';
+	cout << "7. " << s2 << endl;
+	cout << "8. " << s1 << endl;
+	s1 += "mnop";
+	cout << "9. " << s1 << endl;
+	s4 = "qrst-" + s2;
+	cout << "10. " << s4 << endl;
+	s1 = s2 + s4 + " uvw " + "xyz";
+	cout << "11. " << s1 << endl;
+	sort(SArray, SArray + 4);
+	for (int i = 0; i < 4; i++)
+		cout << SArray[i] << endl;
+	//s1的从下标0开始长度为4的子串
+	cout << s1(0, 4) << endl;
+	//s1的从下标5开始长度为10的子串
+	cout << s1(5, 10) << endl;
+	return 0;
+}
